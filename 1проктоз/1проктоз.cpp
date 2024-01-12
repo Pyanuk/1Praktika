31f62d4ee1932f7833d6e70c469dfd21fa4dd6fd
Pyanuk <rhalikov735@gmail.com>
2024-01-12 18:47:45 +0300

    1proktoz

---
+++ 1проктоз/1проктоз.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    cout << "О чем поют воробушки\n";
+    cout << "В последний день зимы?\n";
+    cout << "-Мы выжили!\n";
+    cout << "-Мы выжили!\n";
+    cout << "-Мы живы, живы мы!\n";
+}
